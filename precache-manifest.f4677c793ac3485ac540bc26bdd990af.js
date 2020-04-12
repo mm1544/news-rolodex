872f40e0601f56ba9b4bf571aba2fd1cee08261a
M. Minskis <martynas.minskis@gmail.com>
2020-04-12 20:15:53 +0100

    Updates

---
+++ precache-manifest.f4677c793ac3485ac540bc26bdd990af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1c3173389a2815f1e828a684662999",
+    "url": "/news-rolodex/index.html"
+  },
+  {
+    "revision": "3828ba76592a6cdc888c",
+    "url": "/news-rolodex/static/css/main.3c825473.chunk.css"
+  },
+  {
+    "revision": "6db8ae084f1539e19c58",
+    "url": "/news-rolodex/static/js/2.4c370e1a.chunk.js"
+  },
+  {
+    "revision": "8f448bce97d01acd647a1d4bc48482b1",
+    "url": "/news-rolodex/static/js/2.4c370e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3828ba76592a6cdc888c",
+    "url": "/news-rolodex/static/js/main.721d57d3.chunk.js"
+  },
+  {
+    "revision": "d6c6d51fe7b11104e096",
+    "url": "/news-rolodex/static/js/runtime-main.67acd5b9.js"
+  }
+]);
